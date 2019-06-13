98fb1a90daf00edc0ee085b45a5670c337873fd7
tarek2003 <50145844+tarek2003@users.noreply.github.com>
2019-06-13 05:49:37 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,6 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "568812982734159872") return;
 
   
   if (message.content.startsWith(prefix + 'setwatch')) {
